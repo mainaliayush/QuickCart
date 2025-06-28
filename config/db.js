71acde50c9e733d85dbd6cd6a0e468371687d452
mainaliayush <mainaliayush070@gmail.com>
2025-06-28 16:10:02 -0400

    Typo bug fix again

--- config/db.js
+++ config/db.js
@@ -17,7 +17,7 @@ async function connectDB() {
             bufferCommands: false 
         }
 
-        cached.promise = mongoose.conncet(`${process.env.MONGODB_URI}/Vanguard-ID-Systems`, opts).then( mongoose => {
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/Vanguard-ID-Systems`, opts).then( mongoose => {
             return mongoose
         })
     }
